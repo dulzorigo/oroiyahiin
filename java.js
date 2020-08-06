91333db5881e171bb7a00de40c33ce9f81fab716
Dul Zorigoo <dulzorigoo@Duls-MacBook-Pro.local>
2020-08-07 03:58:42 +0800

    Added tool tip to share button and etc...

--- java.js
+++ java.js
@@ -1,10 +1,19 @@
-    var sites = [
+ var sites = [
         'oroiyaah/sekstsagaanbogd/activity1.html',
         'oroiyaah/zu/activity2.html',
         'oroiyaah/kidult/activity3.html',
-    ];
+        // 'Add other template/activity directory here'
+];
 
-    function randomSite() {
+function randomSite() {
         var i = parseInt(Math.random() * sites.length);
         location.href = sites[i];
-    }
+}
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
